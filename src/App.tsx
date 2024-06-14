782d274605e41a3857b33a9f36e59767dbd2139c
Khrunev Denis <deniskhrunev@gmail.com>
2024-06-14 15:09:49 +0600

    Создан контейнер Jokes и его структура по выводу шуток

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import Movies from './containers/Movies/Movies';
-import './App.css';
+import Jokes from './containers/Jokes/Jokes';
 
 const App = () => {
 
   return (
     <>
       <Movies />
-      <div>
-        asdasdasd
-      </div>
+      <Jokes />
     </>
   );
 };
---
+++ src/containers/Jokes/Jokes.tsx
@@ -0,0 +1,39 @@
+import {useEffect, useState} from 'react';
+import './Jokes.css';
+
+const url = 'https://api.chucknorris.io/jokes/random';
+
+const Jokes = () => {
+  const [joke, setJoke] = useState('');
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+
+      if(response.ok) {
+        const jokes = await response.json();
+        const newPost = jokes.value;
+
+        setJoke(newPost);
+      }
+    } catch (e) {
+      console.error('Ошибка данных ' + e);
+    }
+  };
+
+  useEffect(() => {
+    void fetchData();
+  }, []);
+
+  return (
+    <div className='block-joke'>
+      <h2>Chuck Norris joke</h2>
+      <div className='joke'>
+        {joke}
+      </div>
+      <button className='add-joke' onClick={fetchData}>New joke</button>
+    </div>
+  );
+};
+
+export default Jokes;
